d176408720d84ec7aa8e33f5d102abb374ce94ae
DeployChef <dc.titovvlad@gmail.com>
2023-09-20 20:28:14 +0300

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          leading: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.green,
+            size: 18,
+          ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Чек № 56",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                  color: Color(0xff252849),
+                ),
+              ),
+              Text(
+                "24.02.23 в 12:23",
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 10,
+                  color: Color(0xff60607B),
+                ),
+              ),
+            ],
+          ),
+        ),
         body: Center(
           child: Text('Hello World!'),
         ),
